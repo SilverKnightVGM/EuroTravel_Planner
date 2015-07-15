ce213ed0cc8c2bbc13ab11d4e3d863a59af09378
Emmanuel Colon <emmanuel.colon24@gmail.com>
2015-07-14 21:43:38 -0400

    Set action bar title implemented for options

--- app/src/main/java/com/example/prog2/eurotravelplanner/subdiviciones_opciones.java
+++ app/src/main/java/com/example/prog2/eurotravelplanner/subdiviciones_opciones.java
@@ -3,6 +3,8 @@ package com.example.prog2.eurotravelplanner;
 import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
