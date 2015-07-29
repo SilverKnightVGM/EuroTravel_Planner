42fc47c799929ad116142955acc120a7cc94ceeb
Emmanuel Colon <emmanuel.colon24@gmail.com>
2015-07-29 11:31:05 -0400

    mi metodo se habia borrado

--- app/src/main/java/com/example/prog2/eurotravelplanner/gallery.java
+++ app/src/main/java/com/example/prog2/eurotravelplanner/gallery.java
@@ -1,6 +1,7 @@
 package com.example.prog2.eurotravelplanner;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -45,9 +46,12 @@ public class gallery extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
 
-        final Gallery gallery = (Gallery)findViewById(R.id.gallery);
-        // create adapter gallery
-        gallery.setAdapter(new ImageAdapter(this));
+        Intent recupero_id = getIntent();
+        String ciudad = recupero_id.getStringExtra("id_cuidades");
+        String categoria = recupero_id.getStringExtra("tipo_categoria");
+        String subdivicion = recupero_id.getStringExtra("tipo_subdivision");
+
+        imagenes_Ainsertar(ciudad, categoria, subdivicion);
 
         exp_list = (ExpandableListView)findViewById(R.id.expandableListView);
         lista_lugares = DataProvider.getInfo();//Obtiene el HashMap creado en la clase DataProvider
@@ -84,15 +88,17 @@ public class gallery extends ActionBarActivity {
     public class ImageAdapter extends BaseAdapter {
 
         private Context context;
-        int imagebackground;
+        private Integer[] I_gallery;
+        //int imagebackground;
 
-        public ImageAdapter(Context context){
+        public ImageAdapter(Context context,Integer[] I_gallery){
             this.context=context;
+            this.I_gallery=I_gallery;
         }
 
         @Override
         public int getCount() {
-            return pics.length;
+            return I_gallery.length;
         }
 
         @Override
@@ -108,11 +114,588 @@ public class gallery extends ActionBarActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             ImageView imageView = new ImageView(context);
-            imageView.setImageResource(pics[position]);
+            imageView.setImageResource(I_gallery[position]);
             return imageView;
         }
     }
 
+    public void imagenes_Ainsertar(String ciudad, String categoria, String subdivicion){
 
+        Gallery gallery = (Gallery)findViewById(R.id.gallery);
+
+        // AQUI COMIENZA PARIS
+//-----------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------
+        if(ciudad.equals("paris")){
+
+            if (categoria.equals(getString(R.string.text_transporte))){
+
+                if(subdivicion.equals("Taxi")){
+
+                }
+
+                if(subdivicion.equals("Renta de Autos")){
+
+                }
+                if(subdivicion.equals("Transporte Publico")){
+
+
+                }
+                if(subdivicion.equals("Trenes")){
+
+                }
+
+
+            }
+            if (categoria.equals(getString(R.string.text_gastronomia))){
+
+                if(subdivicion.equals("Restaurantes")){
+
+                    Integer[] p_taxi = {R.drawable.restaurante_paris_epicure, R.drawable.restaurante_paris_seb_on,R.drawable.restaurant_paris_roomies,R.drawable.resturant_paris_pur_jean_francois_rouquette};
+
+                    gallery.setAdapter(new ImageAdapter(this,p_taxi));
+
+                }
+                if(subdivicion.equals("Pasteleria y Panaderia")){
+
+                }
+                if(subdivicion.equals("Comida Rapida")){
+
+                }
+                if(subdivicion.equals("Comida Típicas")){
+
+                }
+
+            }
+
+            if (categoria.equals(getString(R.string.text_hospedaje))){
+
+                if(subdivicion.equals("Hoteles")){
+
+                }
+                if(subdivicion.equals("Hostel")){
+
+                }
+                if(subdivicion.equals("Lugares de Acampar")){
+
+                }
+
+            }
+
+            if (categoria.equals(getString(R.string.text_entretenimiento))){
+
+                if(subdivicion.equals("Bares")){
+
+                }
+                if(subdivicion.equals("Discotecas")){
+
+                }
+                if(subdivicion.equals("Clubes")){
+
+                }
+                if(subdivicion.equals("Parques de Diversión")){
+
+                }
+
+
+            }
+
+            if (categoria.equals(getString(R.string.text_lugarInteres))){
+
+                if(subdivicion.equals("Lugares Historicos")){
+
+                }
+                if(subdivicion.equals("Museos")){
+
+                }
+                if(subdivicion.equals("Playas")){
+
+                }
+                if(subdivicion.equals("Tour por la ciudad")){
+
+                }
+            }
+
+            if (categoria.equals(getString(R.string.text_compras))){
+
+            }
+
+        }
+
+        // AQUI COMIENZA MADIRD
+//-----------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------
+        if(ciudad.equals("madrid")){
+
+            if (categoria.equals(getString(R.string.text_transporte))){
+
+                if(subdivicion.equals("Taxi")){
+
+                }
+
+                if(subdivicion.equals("Renta de Autos")){
+
+                }
+                if(subdivicion.equals("Transporte Publico")){
+
+
+                }
+                if(subdivicion.equals("Trenes")){
+
+                }
+
+
+            }
+            if (categoria.equals(getString(R.string.text_gastronomia))){
+
+                if(subdivicion.equals("Restaurantes")){
+
+                }
+                if(subdivicion.equals("Pasteleria y Panaderia")){
+
+                }
+                if(subdivicion.equals("Comida Rapida")){
+
+                }
+                if(subdivicion.equals("Comida Típicas")){
+
+                }
+
+            }
+
+            if (categoria.equals(getString(R.string.text_hospedaje))){
+
+                if(subdivicion.equals("Hoteles")){
+
+                }
+                if(subdivicion.equals("Hostel")){
+
+                }
+                if(subdivicion.equals("Lugares de Acampar")){
+
+                }
+
+            }
+
+            if (categoria.equals(getString(R.string.text_entretenimiento))){
+
+                if(subdivicion.equals("Bares")){
+
+                }
+                if(subdivicion.equals("Discotecas")){
+
+                }
+                if(subdivicion.equals("Clubes")){
+
+                }
+                if(subdivicion.equals("Parques de Diversión")){
+
+                }
+
+
+            }
+
+            if (categoria.equals(getString(R.string.text_lugarInteres))){
+
+                if(subdivicion.equals("Lugares Historicos")){
+
+                }
+                if(subdivicion.equals("Museos")){
+
+                }
+                if(subdivicion.equals("Playas")){
+
+                }
+                if(subdivicion.equals("Tour por la ciudad")){
+
+                }
+            }
+
+            if (categoria.equals(getString(R.string.text_compras))){
+
+            }
+
+        }
+
+// AQUI COMIENZA ROMA
+//-----------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------
+        if(ciudad.equals("roma")){
+
+            if (categoria.equals(getString(R.string.text_transporte))){
+
+                if(subdivicion.equals("Taxi")){
+
+                }
+
+                if(subdivicion.equals("Renta de Autos")){
+
+                }
+                if(subdivicion.equals("Transporte Publico")){
+
+
+                }
+                if(subdivicion.equals("Trenes")){
+
+                }
+
+
+            }
+            if (categoria.equals(getString(R.string.text_gastronomia))){
+
+                if(subdivicion.equals("Restaurantes")){
+
+                }
+                if(subdivicion.equals("Pasteleria y Panaderia")){
+
+                }
+                if(subdivicion.equals("Comida Rapida")){
+
+                }
+                if(subdivicion.equals("Comida Típicas")){
+
+                }
+
+            }
+
+            if (categoria.equals(getString(R.string.text_hospedaje))){
+
+                if(subdivicion.equals("Hoteles")){
+
+                }
+                if(subdivicion.equals("Hostel")){
+
+                }
+                if(subdivicion.equals("Lugares de Acampar")){
+
+                }
+
+            }
+
+            if (categoria.equals(getString(R.string.text_entretenimiento))){
+
+                if(subdivicion.equals("Bares")){
+
+                }
+                if(subdivicion.equals("Discotecas")){
+
+                }
+                if(subdivicion.equals("Clubes")){
+
+                }
+                if(subdivicion.equals("Parques de Diversión")){
+
+                }
+
+
+            }
+
+            if (categoria.equals(getString(R.string.text_lugarInteres))){
+
+                if(subdivicion.equals("Lugares Historicos")){
+
+                }
+                if(subdivicion.equals("Museos")){
+
+                }
+                if(subdivicion.equals("Playas")){
+
+                }
+                if(subdivicion.equals("Tour por la ciudad")){
+
+                }
+            }
+
+            if (categoria.equals(getString(R.string.text_compras))){
+
+            }
+
+        }
+// AQUI COMIENZA VENECIA
+//-----------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------
+        if(ciudad.equals("venecia")){
+
+            if (categoria.equals(getString(R.string.text_transporte))){
+
+                if(subdivicion.equals("Taxi")){
+
+                }
+
+                if(subdivicion.equals("Renta de Autos")){
+
+                }
+                if(subdivicion.equals("Transporte Publico")){
+
+
+                }
+                if(subdivicion.equals("Trenes")){
+
+                }
+
+
+            }
+            if (categoria.equals(getString(R.string.text_gastronomia))){
+
+                if(subdivicion.equals("Restaurantes")){
+
+                }
+                if(subdivicion.equals("Pasteleria y Panaderia")){
+
+                }
+                if(subdivicion.equals("Comida Rapida")){
+
+                }
+                if(subdivicion.equals("Comida Típicas")){
+
+                }
+
+            }
+
+            if (categoria.equals(getString(R.string.text_hospedaje))){
+
+                if(subdivicion.equals("Hoteles")){
+
+                }
+                if(subdivicion.equals("Hostel")){
+
+                }
+                if(subdivicion.equals("Lugares de Acampar")){
+
+                }
+
+            }
+
+            if (categoria.equals(getString(R.string.text_entretenimiento))){
+
+                if(subdivicion.equals("Bares")){
+
+                }
+                if(subdivicion.equals("Discotecas")){
+
+                }
+                if(subdivicion.equals("Clubes")){
+
+                }
+                if(subdivicion.equals("Parques de Diversión")){
+
+                }
+
+
+            }
+
+            if (categoria.equals(getString(R.string.text_lugarInteres))){
+
+                if(subdivicion.equals("Lugares Historicos")){
+
+                }
+                if(subdivicion.equals("Museos")){
+
+                }
+                if(subdivicion.equals("Playas")){
+
+                }
+                if(subdivicion.equals("Tour por la ciudad")){
+
+                }
+            }
+
+            if (categoria.equals(getString(R.string.text_compras))){
+
+            }
+
+        }
+
+// AQUI COMIENZA BERLIN
+//-----------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------
+        if(ciudad.equals("berlin")){
+
+            if (categoria.equals(getString(R.string.text_transporte))){
+
+                if(subdivicion.equals("Taxi")){
+
+                }
+
+                if(subdivicion.equals("Renta de Autos")){
+
+                }
+                if(subdivicion.equals("Transporte Publico")){
+
+
+                }
+                if(subdivicion.equals("Trenes")){
+
+                }
+
+
+            }
+            if (categoria.equals(getString(R.string.text_gastronomia))){
+
+                if(subdivicion.equals("Restaurantes")){
+
+                }
+                if(subdivicion.equals("Pasteleria y Panaderia")){
+
+                }
+                if(subdivicion.equals("Comida Rapida")){
+
+                }
+                if(subdivicion.equals("Comida Típicas")){
+
+                }
+
+            }
+
+            if (categoria.equals(getString(R.string.text_hospedaje))){
+
+                if(subdivicion.equals("Hoteles")){
+
+                }
+                if(subdivicion.equals("Hostel")){
+
+                }
+                if(subdivicion.equals("Lugares de Acampar")){
+
+                }
+
+            }
+
+            if (categoria.equals(getString(R.string.text_entretenimiento))){
+
+                if(subdivicion.equals("Bares")){
+
+                }
+                if(subdivicion.equals("Discotecas")){
+
+                }
+                if(subdivicion.equals("Clubes")){
+
+                }
+                if(subdivicion.equals("Parques de Diversión")){
+
+                }
+
+
+            }
+
+            if (categoria.equals(getString(R.string.text_lugarInteres))){
+
+                if(subdivicion.equals("Lugares Historicos")){
+
+                }
+                if(subdivicion.equals("Museos")){
+
+                }
+                if(subdivicion.equals("Playas")){
+
+                }
+                if(subdivicion.equals("Tour por la ciudad")){
+
+                }
+            }
+
+            if (categoria.equals(getString(R.string.text_compras))){
+
+            }
+
+        }
+
+// AQUI COMIENZA LONDRES
+//-----------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------
+        if(ciudad.equals("londres")){
+
+            if (categoria.equals(getString(R.string.text_transporte))){
+
+                if(subdivicion.equals("Taxi")){
+
+                }
+
+                if(subdivicion.equals("Renta de Autos")){
+
+                }
+                if(subdivicion.equals("Transporte Publico")){
+
+
+                }
+                if(subdivicion.equals("Trenes")){
+
+                }
+
+
+            }
+            if (categoria.equals(getString(R.string.text_gastronomia))){
+
+                if(subdivicion.equals("Restaurantes")){
+
+                }
+                if(subdivicion.equals("Pasteleria y Panaderia")){
+
+                }
+                if(subdivicion.equals("Comida Rapida")){
+
+                }
+                if(subdivicion.equals("Comida Típicas")){
+
+                }
+
+            }
+
+            if (categoria.equals(getString(R.string.text_hospedaje))){
+
+                if(subdivicion.equals("Hoteles")){
+
+                }
+                if(subdivicion.equals("Hostel")){
+
+                }
+                if(subdivicion.equals("Lugares de Acampar")){
+
+                }
+
+            }
+
+            if (categoria.equals(getString(R.string.text_entretenimiento))){
+
+                if(subdivicion.equals("Bares")){
+
+                }
+                if(subdivicion.equals("Discotecas")){
+
+                }
+                if(subdivicion.equals("Clubes")){
+
+                }
+                if(subdivicion.equals("Parques de Diversión")){
+
+                }
+
+
+            }
+
+            if (categoria.equals(getString(R.string.text_lugarInteres))){
+
+                if(subdivicion.equals("Lugares Historicos")){
+
+                }
+                if(subdivicion.equals("Museos")){
+
+                }
+                if(subdivicion.equals("Playas")){
+
+                }
+                if(subdivicion.equals("Tour por la ciudad")){
+
+                }
+            }
+
+            if (categoria.equals(getString(R.string.text_compras))){
+
+            }
+
+        }
+//----------------------------------------------------------------------------------------------------------------------------------
+    }
 }
 
